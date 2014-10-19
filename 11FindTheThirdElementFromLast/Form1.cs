df98cba867d24ab7d6119f438dacb5ec6085db40
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:48:57 -0700

    Gets the third from last element in a LinkedList
    Finds it in one pass using LinkedListNodes
    UI-Implementation

---
+++ 11FindTheThirdElementFromLast/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _11FindTheThirdElementFromLast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ThirdElementFromLast x = new ThirdElementFromLast(textBox1.Text.Split(','));
+            label2.Text = x.GetThirdElementFromLast();
+        }
+    }
+}
---
+++ 11FindTheThirdElementFromLast/ThirdElementFromLast.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _11FindTheThirdElementFromLast
+{
+    class ThirdElementFromLast
+    {
+        LinkedList<object> list = new LinkedList<object>();
+
+
+        public ThirdElementFromLast(string[] p)
+        {
+            foreach (string str in p) {
+                list.AddLast(Convert.ToInt32(str));
+            }
+        }
+
+        public string GetThirdElementFromLast()
+        {
+            LinkedListNode<object> it1 = list.First;
+            LinkedListNode<object> it2 = list.First;
+            int count = 1;
+            while (it1 != null) {
+                //Needed for it to find it one pass
+                //Plan on return the it2 node so push the it1 node ahead a bit
+                if (count == 1 || count == 2 || count ==3)
+                {
+                    count++;
+                    it1 = it1.Next;
+                }
+                else {
+                    count++;
+                    it1 = it1.Next;
+                    it2 = it2.Next;                
+                }
+            }
+            return it2.Value.ToString();
+        }
+    }
+}
